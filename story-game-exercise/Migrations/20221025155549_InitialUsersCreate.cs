a0e98bad9db4f4ff741a771acbf668f8701e2a78
Michal Pietrowski <pietrowskimichal21@gmail.com>
2022-10-25 18:33:24 +0200

    feat: add books and users migrations

---
+++ story-game-exercise/Migrations/20221025155549_InitialUsersCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace story_game_exercise.Migrations
+{
+    public partial class InitialUsersCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+               name: "Users",
+               columns: table => new
+               {
+                   Id = table.Column<Guid>(),
+                   Nickname = table.Column<string>(),
+                   Password = table.Column<Guid>()
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_Users", x => x.Id);
+               });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Users");
+        }
+    }
+}
---
+++ story-game-exercise/Migrations/20221025155754_InitialBooksCreate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace story_game_exercise.Migrations
+{
+    public partial class InitialBooksCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+               name: "Books",
+               columns: table => new
+               {
+                   Id = table.Column<Guid>(),
+                   Title = table.Column<string>(),
+                   Description = table.Column<string>(),
+                   AuthorId = table.Column<Guid>()
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_Books", x => x.Id);
+               });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Users_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Books");
+        }
+    }
+}
