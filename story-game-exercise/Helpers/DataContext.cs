82c2b64952646de3c06edeb817d66b4f49e853a7
Michal Pietrowski <pietrowskimichal21@gmail.com>
2022-10-25 17:23:37 +0200

    feat: add migrations init

---
+++ story-game-exercise/Helpers/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace story_game_exercise.Helpers
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        //public DbSet<User> Users { get; set; }
+    }
+}
+
